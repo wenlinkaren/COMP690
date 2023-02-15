5fed76ea8e3eaf53047119e4ac8cd714cd0ecce7
Wenlin He <wenlinhe@Wenlins-MacBook-Pro.local>
2023-02-14 20:59:03 -0800

    Two examples of complex/variable expressions

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ let currentMonth
 let summerMonth
 let myLibraryFunctioin
 //STEP 2
+4.5 				// numeric literal 
+'Hello!' 			// string literal 
+true 				// Boolean literal 
+null 				// literal null value
+//STEP 3
+const max = x > y ? x : y;
+const strLength = str.length + getExtraLength(str); 
