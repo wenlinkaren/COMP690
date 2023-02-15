db63e0cdec7dc06841ba9552231eaf1352dec0d6
Wenlin He <wenlinhe@Wenlins-MacBook-Pro.local>
2023-02-14 21:28:44 -0800

    Add a number and a string, the result is coerced

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 //STEP 1
-let someMonth
-function theMonth()
-let currentMonth
-let summerMonth
-let myLibraryFunctioin
+//let someMonth
+//function theMonth()
+//let currentMonth
+//let summerMonth
+//let myLibraryFunctioin
 //STEP 2
-4.5 				// numeric literal 
-'Hello!' 			// string literal 
-true 				// Boolean literal 
-null 				// literal null value
+//4.5 				// numeric literal 
+//'Hello!' 			// string literal 
+//true 				// Boolean literal 
+//null 				// literal null value
 //STEP 3
-const max = x > y ? x : y;
-const strLength = str.length + getExtraLength(str); 
+//const max = x > y ? x : y;
+//const strLength = str.length + getExtraLength(str); 
 //STEP 4
 //let firstName
 //let lastName
@@ -23,11 +23,15 @@ const strLength = str.length + getExtraLength(str);
 //let referralSource
 //let mayWeContactYou
 //STEP 5
-var lastName = "he"
-let address, city
-address = "111 hope road"
-city = "San Diego"
-const state = "CA"
+//var lastName = "he"
+//let address, city
+//address = "111 hope road"
+//city = "San Diego"
+//const state = "CA"
+//STEP 6
+let yourAge = 23
+console.log('This student is ' + yourAge + ' years.')
+
 
 
 
