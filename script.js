60418037cae39e48b3cfc07f266a41fd7f1da015
Wenlin He <wenlinhe@Wenlins-MacBook-Pro.local>
2023-02-14 21:03:58 -0800

    Declare 9 variables by using Camel Casing

--- script.js
+++ script.js
@@ -12,3 +12,14 @@ null 				// literal null value
 //STEP 3
 const max = x > y ? x : y;
 const strLength = str.length + getExtraLength(str); 
+//STEP 4
+let firstName
+let lastName
+let address
+let city
+let state
+let zipCode
+let yourAge
+let referralSource
+let mayWeContactYou
+
